6607a0911f3975fd314dc9d62d40b1f90d787e49
lauCodx <lauphix1@gmail.com>
2024-09-18 21:35:31 +0100

    added the login and the logout button

--- client/google-oath/src/components/LogoutButton.tsx
+++ client/google-oath/src/components/LogoutButton.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function LogoutButton() {
+
+    const handleLogout = () =>{
+        window.location.href = 'http://localhost:3000/auth/logout'
+    }
   return (
-    <div>
-      
+    <div className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded' onClick={handleLogout}>
+      Logout
     </div>
   )
 }
--- client/google-oath/src/components/Profile.tsx
+++ client/google-oath/src/components/Profile.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import LogoutButton from "./LogoutButton";
 
-function Profile() {
-  return (
-    <div>
-      
-    </div>
-  )
+interface User {
+  profilePicture: string;
+  displayName:string;
+  email:string;
 }
 
-export default Profile
+
+function Profile({ user }: { user: User }) {
+    console.log(user.profilePicture)
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg text-center p-6">
+        <img
+          src={user.profilePicture}
+          className="w-24 h-24 rounded-full mx-auto mt-4"
+          alt=""
+        />
+      </div>
+
+      <div className="px-6 py-4">
+        <div className="font-bold text-x1 mb-2">{user.displayName}</div>
+        <div className="text-white-700 text-base">{user.email}</div>
+      </div>
+
+      <LogoutButton/>
+    </div>
+  );
+}
+
+export default Profile;
